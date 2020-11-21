e7830dfb6db744dfab33b3a946837178cd548ea8
Brenna Switzer <brenna.switzer@gmail.com>
2020-11-20 21:44:12 -0800

    Filtering the categories as well

--- src/IncomeStatementTable.js
+++ src/IncomeStatementTable.js
@@ -1,29 +1,34 @@
 import React from "react";
 import {IncomeStatementCategory} from "./IncomeStatementCategory";
+import FilterContext from "./FilterContext";
 
 const IncomeStatementTable = ({data, showPeriod}) => {
     const hasData = data.length > 0;
     if(!hasData) { return <p>No information is available at this time.</p>}
 
     return (
-      <div>
-          <table>
-        {data.map((category) => {
-          return (
-              <>
-                <tr>
-                    <td>{category.name}</td>
-                </tr>
-                  <tr>
-                      <td>
-                          <IncomeStatementCategory category={category} />
-                      </td>
-                  </tr>
-              </>
-          );
-        })}
-          </table>
-      </div>
+      <FilterContext.Consumer>
+          { filters => {
+              return (<div>
+                  {data.map((category) => {
+                      if(category.name && category.name.includes(filters.keyword) || filters.keyword === ""){
+                          return (
+                              <>
+                                  <div>
+                                      <div>{category.name}</div>
+                                  </div>
+                                  <div>
+                                      <div>
+                                          <IncomeStatementCategory category={category} />
+                                      </div>
+                                  </div>
+                              </>
+                          );
+                      }
+                  })}
+              </div>)
+          }}
+      </FilterContext.Consumer>
   );
 }
 export default IncomeStatementTable;
