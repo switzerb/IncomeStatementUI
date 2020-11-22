f4e81167923c279edca4ae5e79845d7ea9e69a08
Brenna Switzer <brenna.switzer@gmail.com>
2020-11-21 17:07:23 -0800

    Tighten up the dropdown

--- src/MonthSelect.js
+++ src/MonthSelect.js
@@ -4,6 +4,7 @@ import "./MonthSelect.css";
 
 const MonthSelect = ({options, onClick}) => {
     const [open, setOpen] = useState(false);
+    const [selected, setSelected] = useState("");
 
     const toggleOpen = () => {
         setOpen(!open);
@@ -11,37 +12,38 @@ const MonthSelect = ({options, onClick}) => {
 
     const handleSelect = (option) => {
         onClick(option);
+        setSelected(option);
         setOpen(false);
     }
 
     const handleClear = () => {
         onClick("");
+        setSelected("")
         setOpen(false);
     }
 
     return (
         <div className="MonthSelect">
+            <div className="MonthSelect-label">Select Time Range</div>
             <div
                 onClick={toggleOpen}
-                className="MonthSelect-label"
+                className="MonthSelect-selected"
             >
-                Filter By Financial Period
-            </div>
-            <div
-                onClick={toggleOpen}
-                className="MonthSelect-toggle"
-            >
-                { open ? "^" : "v" }
-            </div>
-            { open
-                ? <div className="MonthSelect-options">
-                    <div onClick={handleClear}>
-                        Show Quarter
-                    </div>
-                    {options.map(option => <div key={option} onClick={() => handleSelect(option)}>{option}</div>)}
+                <div
+                    onClick={toggleOpen}
+                    className="MonthSelect-toggle"
+                >
+                    { open ? "^" : "v" }
                 </div>
-                : null
-            }
+                {selected === "" ? "Fiscal Quarter" : selected }
+                { open
+                    ? <div className="MonthSelect-options">
+                        <div onClick={handleClear}>Fiscal Quarter</div>
+                        {options.map(option => <div key={option} onClick={() => handleSelect(option)}>{option}</div>)}
+                    </div>
+                    : null
+                }
+            </div>
         </div>);
 }
 
